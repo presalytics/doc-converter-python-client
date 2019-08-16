42c513fa92691128416fbe718562a9c410a56148
Kevin Hannegan <kevin@chart-a-lot.com>
2019-08-16 05:02:28 +0000

    Automated patch update, version 0.1.3 from presalytics codegen

--- python-client/presalytics_doc_converter/__init__.py
+++ python-client/presalytics_doc_converter/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.1.21"
+__version__ = "0.1.3"
 
 # import apis into sdk package
 from presalytics_doc_converter.api.default_api import DefaultApi
